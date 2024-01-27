2b7622c06c819d878e7ce6b2fa54c1b5d1142fb6
HeyBroooo <mohitkumar4049a@gmail.com>
2024-01-27 17:09:37 +0530

    Next ui nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,79 +1,98 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-
-
-
+import React from "react";
+import {
+  Navbar as NextUINavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+  Link,
+  Button,
+} from "@nextui-org/react";
+// import { AcmeLogo } from "./AcmeLogo.jsx";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
-
-  const links = [
-    {
-      id: 1,
-      link: "home",
-    },
-    {
-      id: 2,
-      link: "about",
-    },
-    {
-      id: 3,
-      link: "portfolio",
-    },
-    {
-      id: 4,
-      link: "experience",
-    },
-    {
-      id: 5,
-      link: "contact",
-    },
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
   ];
 
+
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 fixed backdrop-filter backdrop-blur-lg bg-opacity-30 border border-gray-200">
-      <div>
-        {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="">Logo</a></h1> */}
-        <Image src="/portlogo.png" alt="logo" width={50} height={50} />
-      </div>
+    <NextUINavbar disableAnimation isBordered>
+    <NavbarContent className="sm:hidden" justify="start">
+      <NavbarMenuToggle />
+    </NavbarContent>
 
-      <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-black duration-200 link-underline"
+    <NavbarContent className="sm:hidden pr-3" justify="center">
+      <NavbarBrand>
+        {/* <AcmeLogo /> */}
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+    </NavbarContent>
+
+    <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarBrand>
+        {/* <AcmeLogo /> */}
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarItem>
+        <Link color="foreground" href="#">
+          Features
+        </Link>
+      </NavbarItem>
+      <NavbarItem isActive>
+        <Link href="#" aria-current="page" color="warning">
+          Customers
+        </Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link color="foreground" href="#">
+          Integrations
+        </Link>
+      </NavbarItem>
+    </NavbarContent>
+
+    <NavbarContent justify="end">
+      <NavbarItem className="hidden lg:flex">
+        <Link href="#">Login</Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Button as={Link} color="warning" href="#" variant="flat">
+          Sign Up
+        </Button>
+      </NavbarItem>
+    </NavbarContent>
+
+    <NavbarMenu>
+      {menuItems.map((item, index) => (
+        <NavbarMenuItem key={`${item}-${index}`}>
+          <Link
+            className="w-full"
+            color={
+              index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+            }
+            href="#"
+            size="lg"
           >
-            <Link href={link}>{link}</Link>
-          </li>
-        ))}
-      </ul>
-
-      <div
-        onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-950 md:hidden"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
-
-      {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-950 text-gray-500">
-          {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="px-4 cursor-pointer capitalize py-6 text-4xl"
-            >
-              <Link onClick={() => setNav(!nav)} href={link}>
-                {link}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+            {item}
+          </Link>
+        </NavbarMenuItem>
+      ))}
+    </NavbarMenu>
+  </NextUINavbar>
   );
 };
+
 
 export default Navbar;
